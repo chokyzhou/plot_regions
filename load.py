6bb79558ff925837280e4167d63c7ac2a2cfd860
choky <62416632+chokyzhou@users.noreply.github.com>
2021-03-29 03:14:27 -0700

    Delete load.py

--- load.py
+++
@@ -1,8 +0,0 @@
-from get_token import get_token
-from lookup_beatmap import lookup_beatmap
-
-def load():
-    id = 6093
-    secret = 'Q90UKox03XxD5QNAMNfbTbjexo5fJ02ljiYCinfS'
-    grant_type = 'client_credentials'
-    return id,secret,grant_type
